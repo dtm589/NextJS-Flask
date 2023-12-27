80a88b02b32b5e0991b971f1e0177e4522cfac41
Derek Marchese <derekmarchese1@gmail.com>
2023-12-27 17:23:45 -0500

    mapping over array and rendering to page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,21 +1,31 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 
 function index() {
 
   const [message, setMessage] = useState('Loading');
+  const [people, setPeople] = useState([]);
 
-  useEffect(() =>{
+  useEffect(() => {
     fetch("http://localhost:8080/api/home").then(
       response => response.json()
     ).then(
       data => {
         setMessage(data.message)
+        setPeople(data.people)
       }
     )
   }, []);
 
   return (
-    <div>{message}</div>
+    <div>
+      <div>{message}</div>
+
+      {people && people.map((person, index) => (
+        <div key={index}>
+          {person}
+        </div>
+      ))}
+    </div>
   )
 };
 
