e6ce4d2146383c0598511f5cb5bce940cd3fb922
Derek Marchese <derekmarchese1@gmail.com>
2023-12-27 16:53:12 -0500

    flask cors

--- server/server.py
+++ server/server.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 #app instance
 app = Flask(__name__)
+CORS(app)
 
 #routes
 @app.route("/api/home", methods=['GET'])
@@ -12,5 +14,5 @@ def return_home():
 
 if __name__ == "__main__":
     #only have debug set to true if developing, if deploying remove
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
     
